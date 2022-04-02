24ca297eff8c9dd127234e5aa15629825b64b82e
Vinay Kumar <vinaykumar1908@gmail.com>
2022-04-02 12:14:32 +0530

    removed dispatch

--- cbw/settings.py
+++ cbw/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'dispatch.apps.DispatchConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
 ]
